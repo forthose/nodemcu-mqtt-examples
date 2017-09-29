2609bd49b3cf61ca128e7b01a1d393480470bde9
forthose <32369866+forthose@users.noreply.github.com>
2017-09-29 20:31:57 +0200

    Initial commit

---
+++ iotLamp.lua
@@ -0,0 +1,47 @@
+conf = require("config");
+m = nil;
+connAttempts = conf.connectionAttempts; 
+
+function main()
+  enduser_setup.start(start, function(err, str) print("Error: "..err.." "..str) end);
+end
+
+function start()
+  print("Connected to wifi as:" .. wifi.sta.getip())
+  gpio.mode(conf.outputPin,gpio.OUTPUT);
+  mqtt = mqtt.Client("MQTT"..node.chipid(), 120, conf.username, conf.password);
+  net.cert.verify(conf.certificate);
+  connectToBroker();
+  mqtt:on("connect", function(client) 
+    mqtt:subscribe(conf.lampTopic, 0, function(conn) print("subscribe success") end)
+  end);
+  mqtt:on("connected", function(client) print("connected") end);
+  mqtt:on("message",function(client, topic, message)
+    if (topic == conf.lampTopic) then
+      toggleLamp(message); 
+    end;    
+  end)
+end
+
+function connectToBroker()
+  if(connnAttempts ~= 0) then
+    mqtt:connect(conf.host, conf.port, 1, 0, function(con) print(con) end, handleMqttError);
+  else
+    node.dsleep(sleepTime*1000000, 1);   
+  end
+end
+
+function handleMqttError(client, reason)
+   connAttempts = conncectionAttempts - 1;
+  tmr.create():alarm(2*1000, tmr.ALARM_SINGLE, connectToBroker);
+end
+
+function toggleLamp(message) 
+  if (message == "true") then
+    gpio.write(conf.outputPin, gpio.HIGH);
+  else
+    gpio.write(conf.outputPin, gpio.LOW);
+  end
+end
+
+main();
---
+++ iotSensor.lua
@@ -0,0 +1,54 @@
+conf = require("config");
+m = nil;
+connAttempts = conf.connectionAttempts; 
+
+function main()
+  enduser_setup.start(start,function(err, str) print("Error: "..err.." "..str) end);
+end
+
+function start()
+  print("Connected to wifi as:" .. wifi.sta.getip())
+  bmp085.init(1,2);
+  mqtt = mqtt.Client("MQTT"..node.chipid(), 120, conf.username, conf.password);
+  net.cert.verify(conf.certificate);
+  connectToBroker();
+  mqtt:on("connect",function(client)
+    print ("connected");
+    sendData()
+  end)
+end
+
+function connectToBroker()
+  if(connnAttempts ~= 0) then
+    mqtt:connect(conf.host, conf.port, 1, 0, function(con) print(con) end, handleMqttError);
+  else
+    node.dsleep(sleepTime*1000000, 1);   
+  end
+end
+
+function handleMqttError(client, reason)
+   connAttempts = conncectionAttempts - 1;
+  tmr.create():alarm(2*1000, tmr.ALARM_SINGLE, connectToBroker);
+end
+
+function sendData()
+  local temp,pres = nil;
+  print ("sending");
+  temp = bmp085.temperature();
+  pres = bmp085.pressure();
+  if(temp ~= nil and pres ~= nil) then
+    mqtt:publish(conf.temperatureTopic, (temp/10)..'.'..(temp%10), 0, 1);
+    mqtt:publish(conf.pressureTopic, (pres/100)..'.'..(pres%100), 0, 1, function()
+      tmr.create():alarm(1*1000, tmr.ALARM_SINGLE, closeAndSleep);
+    end);
+  else
+    closeAndSleep();
+  end
+end
+
+function closeAndSleep()
+  mqtt:close();
+  node.dsleep(sleepTime*1000000, 1);
+end
+
+main();
